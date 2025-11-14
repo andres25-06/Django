e833ed52b01247b0d283b9160260d77d6f1a88b1
Andres <eiderpira880@gmail.com>
2025-11-14 10:26:09 -0500

    Se hice cambios

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Member(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
-    phone=models.IntegerField(null=True)
+    phone = models.IntegerField(null=True)
     joined_date = models.DateField(null=True)
-    
+    direction = models.CharField(null=True)
+    tipe_sexo = models.CharField(null=True)
+    number_document = models.IntegerField(null=True)
